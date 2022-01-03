f2b7786d91edec1b5d613de7bae98159a6676bfa
LeonV <leonvv25@gmail.com>
2022-01-03 12:53:20 +0200

    commit VF_start

---
+++ src/js/parts/test_file.js
@@ -0,0 +1,4 @@
+function test_func () {
+    console.log('test JS');
+}
+module.exports = test_func;
---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+// require('es6-promise').polyfill();
+require('nodelist-foreach-polyfill');
+require('formdata-polyfill');
+
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+    //test
+    let test_func = require('./parts/test_file');
+    test_func();
+
+    //working script
+
+    
+    
+    
+
+});
+
